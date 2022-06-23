f6d6c68b62a4c54de335fabcdb1fb5e036517173
Olexsii <realcarter50@gmail.com>
2022-06-24 00:14:43 +0300

    slider setting

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,27 +10,42 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 0
+    var targetValue = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startNewRound()
     }
 
     @IBAction func showAlert() {
+        
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+
         let alert = UIAlertController(title: "Hello World",
-                                      message: "This my first app",
+                                      message: message,
                                       preferredStyle: .alert)
         
-        let alertAction = UIAlertAction(title: "Awesome",
+        let alertAction = UIAlertAction(title: "Ok",
                                         style: .default)
         
         alert.addAction(alertAction)
         present(alert, animated: true)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ sender: UISlider) {
         currentValue = lroundf(sender.value)
+        targetLabel.text = "\(lroundf(sender.value))"
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 }
 
