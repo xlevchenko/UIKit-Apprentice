b67d472628ff515d75b13a7ed79f301531d8bf63
Olexsii <realcarter50@gmail.com>
2022-07-08 15:20:47 +0300

    chapter 12

--- Checklists/Checklists/AddItemViewController.swift
+++ Checklists/Checklists/AddItemViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        textField.delegate = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -32,7 +33,28 @@ class AddItemViewController: UITableViewController {
         navigationController?.popViewController(animated: true)
     }
     
+    
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
+    }
+    
+    
+    //MARK: - UITextFieldDelegate
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let startingRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: startingRange, with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+      doneBarButton.isEnabled = false
+      return true
     }
 }
