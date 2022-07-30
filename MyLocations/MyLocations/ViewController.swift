4cb7345ac5d23f1bee2935d0b15e98ad62d0fc46
Olexsii <realcarter50@gmail.com>
2022-07-30 12:02:13 +0300

    MyLocations

---
+++ MyLocations/MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Olexsii Levchenko on 7/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
