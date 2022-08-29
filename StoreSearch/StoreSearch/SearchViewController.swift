6c85b2a078a88beb5b4519847f7742f7fbeec53f
Olexsii <realcarter50@gmail.com>
2022-08-29 12:23:53 +0300

    create new project Search Bar

---
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Olexsii Levchenko on 8/29/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
